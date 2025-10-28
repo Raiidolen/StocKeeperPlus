36b7775b6c5ca10154368ff9b21c0434a09c8a99
Tom <tommalexandre@gmail.com>
2025-10-28 10:55:56 +0100

    Creation of the middleware

--- API/route/product.js
+++ API/route/product.js
@@ -7,11 +7,14 @@ import {
     getAllProduct,
     deleteProduct
 }  from  '../controler/productORM.js'
+import {productValidatorMiddlewares as PVM} from '../middleware/validation.js';
 
 const router = Router();
 
-router.post('/', addProduct);
-router.patch('/', updateProduct);
-router.get('/:id', getProduct);
+router.post('/', PVM.productToAdd, addProduct);
+router.patch('/', PVM.productToUpdate, updateProduct);
+router.get('/:id', PVM.searchedProduct, getProduct);
 router.get('/', getAllProduct);
-router.delete('/:id', deleteProduct);
+router.delete('/:id', PVM.productToDelete, deleteProduct);
+
+export default router;
