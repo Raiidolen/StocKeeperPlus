9fdc63c39f67366353e28698c7b48cc5530fd8f5
VerhaegheMartin <etu51486@henallux.be>
2025-11-04 15:52:22 +0100

    correction erreurs middlewares store

--- API/route/store.js
+++ API/route/store.js
@@ -8,7 +8,7 @@ import {
     getAllStores, 
     getStoresWithInRange
 } from '../controller/storeORM.js';
-import {storeValidatorMiddlewares as SVM} from '../middleware/validation.js';
+import {storeValidatorMiddlewares as SVM} from '../middleware/validationStore.js';
 
 const router = Router();
 
