1d34719ec32407ec6f51c906e10ff64e43921c03
VerhaegheMartin <etu51486@henallux.be>
2025-11-04 15:09:22 +0100

    add store

--- API/route/index.js
+++ API/route/index.js
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import {default as authRouter} from './auth.js'
 import {default as foodRouter} from './food.js'
+import {default as storeRouter } from './store.js';
 import 'dotenv/config';
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use('/food', foodRouter);
-
+router.use('/store', storeRouter);
 
 export default router;
