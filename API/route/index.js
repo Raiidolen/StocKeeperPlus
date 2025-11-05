0d6db77e9cbd1181c631e7fbb569c9a26b77df9b
VerhaegheMartin <etu51486@henallux.be>
2025-11-05 16:07:05 +0100

    add foodStore

--- API/route/index.js
+++ API/route/index.js
@@ -4,6 +4,7 @@ import {default as userRouter} from './user.js'
 import {default as foodRouter} from './food.js'
 import {default as storeRouter} from './store.js'
 import {default as foodUserRouter} from './foodUser.js'
+import {default as foodStoreRouter} from './foodStore.js'
 import 'dotenv/config';
 const router = Router();
 
@@ -12,4 +13,5 @@ router.use('/user', userRouter);
 router.use('/food', foodRouter);
 router.use('/store', storeRouter);
 router.use('/foodUser', foodUserRouter);
+router.use('/foodStore', foodStoreRouter);
 export default router;
--- API/route/store.js
+++ API/route/store.js
@@ -12,11 +12,11 @@ import {storeValidatorMiddlewares as SVM} from '../middleware/storeValidation.js
 
 const router = Router();
 
-router.get('/range', SVM.storeToGetInRange, getStoresWithInRange);
-router.get('/', SVM.storeToGetAll, getAllStores);
-router.get('/:id', SVM.storeToGet, getStore);
+router.get('/', SVM.storeToGetInRange, getStoresWithInRange);
+router.get('/all', SVM.storeToGetAll, getAllStores);
+router.get('/', SVM.storeToGet, getStore);
 router.post('/', SVM.storeToAdd, addStore);
-router.put('/:id', SVM.storeToUpdate, updateStore);
-router.delete('/:id', SVM.storeToDelete, deleteStore);
+router.put('/', SVM.storeToUpdate, updateStore);
+router.delete('/', SVM.storeToDelete, deleteStore);
 
 export default router;
