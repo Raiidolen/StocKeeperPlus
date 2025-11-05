39d85d1dba86f4ed648d73f12ca4eb9cef6dd5ec
VerhaegheMartin <etu51486@henallux.be>
2025-11-05 13:17:43 +0100

    add store in route

--- API/route/index.js
+++ API/route/index.js
@@ -3,6 +3,7 @@ import {default as authRouter} from './auth.js'
 import {default as userRouter} from './user.js'
 import {default as foodRouter} from './food.js'
 import {default as recipeRouter} from './recipe.js'
+import {default as storeRouter} from './store.js'
 import {default as foodUserRouter} from './foodUser.js'
 import 'dotenv/config';
 const router = Router();
@@ -11,6 +12,7 @@ router.use("/auth", authRouter);
 router.use('/user', userRouter);
 router.use('/food', foodRouter);
 router.use('/recipe', recipeRouter);
+router.use('/store', storeRouter);
 router.use('/foodUser', foodUserRouter);
 
 export default router;
