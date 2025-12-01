ee1bebcb6a3076fb8df58f51cd6ac8e28750efd3
AuguC <zerix2909@gmail.com>
2025-12-01 10:54:40 +0100

    Switch from bearer to cookies

--- API/controller/auth.js
+++ API/controller/auth.js
@@ -30,7 +30,14 @@ export const login = async (req, res) => {
     const payload = { email: person.email, role: person.role };
     const jwt = sign(payload, { expiresIn: "8h" });
 
-    res.status(200).json({ token: jwt });
+    res.cookie("jwt", jwt, {
+      httpOnly: true,
+      secure:false,
+      sameSite: "Strict",
+      maxAge: 12 * 60 * 60 * 1000 // car le temps est en mili sec
+    })
+
+    res.status(200);
   } catch (err) {
     console.error("Erreur login :", err);
     res.sendStatus(500);
