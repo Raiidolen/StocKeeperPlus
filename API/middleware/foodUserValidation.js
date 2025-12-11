1b3047842d0588e3bea9aa67d654f159f981bba0
Tom <tommalexandre@gmail.com>
2025-12-11 21:14:02 +0100

    changes

--- API/middleware/foodUserValidation.js
+++ API/middleware/foodUserValidation.js
@@ -12,6 +12,7 @@ export const foodUserValidatorMiddleware = {
     },
     foodUserToAdd: async(req, res, next) => {
         try {
+            console.log(req.body)
             req.val  = await foodUserValidator.foodUserToAdd.validate(req.body);
             req.val.food_id = req.val.food;
             next();
