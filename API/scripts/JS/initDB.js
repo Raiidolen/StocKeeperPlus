8cf38963a71c3bbb17e3bbc30f9fe4dc4fe0dd34
Bokode <tommalexandre@gmail.com>
2025-12-19 15:36:19 +0100

    FillingDB

--- API/scripts/JS/initDB.js
+++ API/scripts/JS/initDB.js
@@ -1,14 +1,21 @@
 import {readFileSync} from "node:fs";
 import {pool} from "../../database/database.js";
 
-const requests = readFileSync(
+const initDB = readFileSync(
     './API/scripts/SQL/initDB.sql',
     {encoding: "utf-8"}
 );
 
+const fillingDB = readFileSync(
+    './API/scripts/SQL/fillingDB.sql',
+    {encoding: "utf-8"}
+);
+
 try {
-    await pool.query(requests, []);
+    await pool.query(initDB, []);
     console.log("Init done");
+    await pool.query(fillingDB, []);
+    console.log("Filling done");
 } catch (e) {
     console.error(e);
 }
